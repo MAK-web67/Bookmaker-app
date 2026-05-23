ab42a57fa1c612615857a31da3a3d9d7a2a198f3
MAK-web67 <leomak320@gmail.com>
2026-05-23 08:41:32 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,6 @@
+// v2
 const handler = async (req, res) => {
+
 res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
 res.setHeader(‘Access-Control-Allow-Methods’, ‘POST, OPTIONS’);
 res.setHeader(‘Access-Control-Allow-Headers’, ‘Content-Type’);
